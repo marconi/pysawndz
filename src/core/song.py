073635520faa0d1f4d916c1cd154e8e30598d105
Marconi Moreto Jr <caketoad@gmail.com>
2009-09-19 02:46:54 -0700

    Fixed song's class autodiscovery

--- src/core/song.py
+++ src/core/song.py
@@ -37,36 +37,73 @@ class Song(object):
     def __saveSong(self):
         query  = QSqlQuery(db)
         
-        print self.__getSongArtist(self.__artist)
-        return
-        
-        query.prepare("INSERT INTO songs(title, duration) VALUES(?, ?)")
+        query.prepare("INSERT INTO songs(title, duration, artist_id, genre_id, album_id) VALUES(?, ?, ?, ?, ?)")
         query.addBindValue(QVariant(self.__title))
         query.addBindValue(QVariant(self.__duration))
+        artist_id = self.__getSongArtist()
+        query.addBindValue(QVariant(artist_id))
+        query.addBindValue(QVariant(self.__getSongGenre()))
+        query.addBindValue(QVariant(self.__getSongAlbum(artist_id)))                         
         if query.exec_():
-            self.__id = query.lastInsertId().toInt()
+            self.__id = query.lastInsertId().toInt()[0]
         else:
             print query.lastError().text()
 
-    def __getSongArtist(self, artist):
+    def __getSongArtist(self):
         query  = QSqlQuery(db)
-        if query.exec_(QString("SELECT id FROM artists WHERE name = %1").arg(artist)):
-            if query.size() > 0:
-                row = query.record()
-                return row.value(1).toInt()
+        sql = QString("SELECT id FROM artists WHERE name = '%1'").arg(self.__artist)
+        if query.exec_(sql):
+            if query.size() > 0 and query.next():
+                artist_id = query.value(0).toInt()[0]
+                return artist_id
             else:
                 query.prepare("INSERT INTO artists(name) VALUES(?)")
-                query.addBindValue(QVariant(artist))
+                query.addBindValue(QVariant(self.__artist))
                 if query.exec_():
-                    return query.lastInsertId().toInt()
+                    return query.lastInsertId().toInt()[0]
                 else:
                     print query.lastError().text()
         else:
             print query.lastError().text()
+            
+    def __getSongGenre(self):
+        query  = QSqlQuery(db)
+        sql = QString("SELECT id FROM genres WHERE name = '%1'").arg(self.__genre)
+        if query.exec_(sql):
+            if query.size() > 0 and query.next():
+                genre_id = query.value(0).toInt()[0]
+                return genre_id
+            else:
+                query.prepare("INSERT INTO genres(name) VALUES(?)")
+                query.addBindValue(QVariant(self.__genre))
+                if query.exec_():
+                    return query.lastInsertId().toInt()[0]
+                else:
+                    print query.lastError().text()
+        else:
+            print query.lastError().text()
+            
+    def __getSongAlbum(self, artist_id):
+        query  = QSqlQuery(db)
+        sql = QString("SELECT id FROM albums WHERE name = '%1' AND artist_id = %2").arg(self.__album).arg(artist_id)
+        if query.exec_(sql):
+            if query.size() > 0 and query.next():
+                album_id = query.value(0).toInt()[0]
+                return album_id
+            else:
+                query.prepare("INSERT INTO albums(name, artist_id) VALUES(?, ?)")
+                query.addBindValue(QVariant(self.__album))
+                query.addBindValue(QVariant(artist_id))
+                if query.exec_():
+                    return query.lastInsertId().toInt()[0]
+                else:
+                    print query.lastError().text()
+        else:
+            print query.lastError().text()
+        
 
 if __name__ == "__main__":
-    #app = QApplication(sys.argv)
-    
+    #testsuite
     db = QSqlDatabase("QMYSQL")
     db.setDatabaseName("pysawndz")
     db.setUserName("marc")
@@ -76,9 +113,12 @@ if __name__ == "__main__":
         print db.lastError().text()
         sys.exit(1)
     
-    song = Song(title="title101", duration="12345", artist="artist101")
+    song = Song(title="Du Hast", duration="12345", artist="Rammstein", genre="Rock", album="Berlin")
     print song.getId()
-    #sys.exit(app.exec_())
+    song = Song(title="Tequero Puta", duration="54321", artist="Rammstein", genre="Rock", album="Berlin")
+    print song.getId()
+    song = Song(title="Fuer Fuer", duration="2345", artist="Ramstein", genre="Rock", album="Berlin")
+    print song.getId()
     
         
     
